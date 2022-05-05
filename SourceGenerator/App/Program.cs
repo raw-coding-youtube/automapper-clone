60c43ee543ab6be22f7a32f682f470130354280a
T0shik <antonwieslander@hotmail.com>
2022-05-05 21:30:48 +0100

    setup

---
+++ SourceGenerator/App/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+public class A
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class B
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ SourceGenerator/Generator/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Generator;
+
+[Generator]
+public class HelloSourceGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+}
