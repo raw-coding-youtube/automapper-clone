8f3cf6b37fba946eadaa5dea11be2433408827d7
T0shik <antonwieslander@hotmail.com>
2022-05-07 16:58:58 +0100

    source gen solution

--- SourceGenerator/App/Program.cs
+++ SourceGenerator/App/Program.cs
@@ -1,16 +1,23 @@
-﻿
+﻿var b = Mapper.Map(new A { Id = 1, Name = "Foo" });
 
+Console.WriteLine(b.Id + " name: " + b.Name + " word: " + b.Word);
 
-
-
+[Map]
 public class A
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Word { get; set; } = "asdfasdfdsfsafsad";
 }
 
+[Map]
 public class B
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Word { get; set; } = "a";
+}
+
+public class MapAttribute : Attribute
+{
 }
--- SourceGenerator/Generator/Generator.cs
+++ SourceGenerator/Generator/Generator.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Generator;
 
@@ -7,9 +13,83 @@ public class Generator : ISourceGenerator
 {
     public void Execute(GeneratorExecutionContext context)
     {
+        var receiver = (ClassSyntaxReceiver)context.SyntaxReceiver;
+
+        var sb = new StringBuilder();
+        sb.Append("public static class Mapper {");
+
+        for (int i = 0; i < receiver.ClassMappings.Count; i++)
+        {
+            var from = receiver.ClassMappings[i];
+
+            for (int j = 0; j < receiver.ClassMappings.Count; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                var to = receiver.ClassMappings[j];
+                sb.Append("public static ");
+                sb.Append(to.Name);
+                sb.Append(" Map(");
+                sb.Append(from.Name);
+                sb.Append(" from){");
+                sb.Append("return new(){");
+                
+                foreach (var fromProperty in from.Properties)
+                {
+                    var toProperty = to.Properties.FirstOrDefault(x => x == fromProperty);
+                    if (toProperty == null)
+                    {
+                        continue;
+                    }
+                        
+                    sb.Append(toProperty);
+                    sb.Append("=from.");
+                    sb.Append(fromProperty);
+                    sb.Append(',');
+                }
+                
+                sb.Append("};");
+                sb.Append('}');
+            }
+        }
+
+        sb.Append('}');
+        
+        context.AddSource("Mapper.g.cs", sb.ToString());
     }
 
     public void Initialize(GeneratorInitializationContext context)
     {
+        context.RegisterForSyntaxNotifications(() => new ClassSyntaxReceiver());
     }
 }
+
+public class ClassSyntaxReceiver : ISyntaxReceiver
+{
+    public List<Clazz> ClassMappings { get; set; } = new();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is not ClassDeclarationSyntax cds)
+        {
+            return;
+        }
+
+        if (!cds.AttributeLists.Any(x => x.Attributes.Any(y => y.Name.ToString() == "Map")))
+        {
+            return;
+        }
+
+        var properties = cds.Members.Select(x => x as PropertyDeclarationSyntax)
+            .Where(x => x != null)
+            .Select(x => x.Identifier.Text)
+            .ToList();
+
+        ClassMappings.Add(new(cds.Identifier.Text, properties));
+    }
+}
+
+public record Clazz(string Name, List<string> Properties);
